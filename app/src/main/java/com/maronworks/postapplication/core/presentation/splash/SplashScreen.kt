a836f61e9108aaa4691113f36a4475898213663a
ralphmaron <ralphmaron.dev@gmail.com>
2024-03-12 21:30:11 +0800

    edited splash

--- app/src/main/java/com/maronworks/postapplication/core/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/maronworks/postapplication/core/presentation/splash/SplashScreen.kt
@@ -1,26 +1,34 @@
 package com.maronworks.postapplication.core.presentation.splash
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.maronworks.postapplication.R
 import com.maronworks.postapplication.core.navigation.Screens
 import kotlinx.coroutines.delay
@@ -43,20 +51,41 @@ private fun Splash() {
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    listOf(
+                        Color(241, 211, 241),
+                        Color(192, 173, 250)
+                    )
+                )
+            )
     ) {
         Box(
             modifier = Modifier
                 .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.app_logo),
-                contentDescription = "",
-                modifier = Modifier
-                    .size(200.dp)
-                    .clip(RoundedCornerShape(8.dp)),
-                contentScale = ContentScale.Crop
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.app_logo),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(200.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    text = "POST APPLICATION",
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight.W600,
+                    fontSize = 20.sp,
+                    color = Color(78, 3, 78)
+                )
+            }
         }
         Box(
             modifier = Modifier
@@ -73,13 +102,15 @@ private fun Splash() {
                     text = "Developed by:",
                     fontFamily = FontFamily.Monospace,
                     fontWeight = FontWeight.W500,
-                    fontSize = 12.sp
+                    fontSize = 12.sp,
+                    color = Color(78, 3, 78)
                 )
                 Text(
                     text = "Ralph Maron Eda",
                     fontFamily = FontFamily.Monospace,
                     fontWeight = FontWeight.W600,
-                    fontSize = 14.sp
+                    fontSize = 14.sp,
+                    color = Color(78, 3, 78)
                 )
             }
         }
