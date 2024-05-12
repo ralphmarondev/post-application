1156473c1d77378bd76f8aea544a52cea0f416a6
ralphmaron <ralphmaron.dev@gmail.com>
2024-05-12 10:17:17 +0800

    show bottom sheet on more

--- app/src/main/java/com/maronworks/postapplication/home/feed/Feed.kt
+++ app/src/main/java/com/maronworks/postapplication/home/feed/Feed.kt
@@ -1,7 +1,9 @@
 package com.maronworks.postapplication.home.feed
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,15 +12,21 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Mail
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.maronworks.postapplication.R
@@ -29,6 +37,8 @@ import com.maronworks.postapplication.home.model.post.PostModel
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Feed() {
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -66,11 +76,11 @@ fun Feed() {
             item {
                 Spacer(modifier = Modifier.height(10.dp))
             }
-            items(2){
+            items(2) {
                 val post = PostModel(
                     name = "Ralph Maron Eda",
                     image = R.drawable.cute_me,
-                    post = "Hello there, Ralph Maron Eda is here. He is a compute Enigneering student is passionate in learning new technologies. He likes mobile development, so as web development. He is also interested in machine learning and artificial intelligence.",
+                    post = "Hello there, Ralph Maron Eda is here. He is a compute Engineering student is passionate in learning new technologies. He likes mobile development, so as web development. He is also interested in machine learning and artificial intelligence.",
                     date = "2024-05-11 | 7:58"
                 )
                 PostCard(
@@ -80,11 +90,37 @@ fun Feed() {
                     post = post,
                     onMore = {
                         // open bottom sheet
+                        showBottomSheet = !showBottomSheet
                     }
                 )
             }
             item {
                 Spacer(modifier = Modifier.height(100.dp))
+            }
+        }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(onDismissRequest = { showBottomSheet = !showBottomSheet }) {
+                Column(
+                    modifier = Modifier
+                        .defaultMinSize(minHeight = 500.dp)
+                        .padding(start = 15.dp, end = 15.dp, top = 20.dp, bottom = 150.dp)
+                ) {
+                    repeat(2) {
+                        ElevatedCard(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(5.dp)
+                        ) {
+                            Text(
+                                text = "Hello there!",
+                                modifier = Modifier
+                                    .padding(10.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
     }
